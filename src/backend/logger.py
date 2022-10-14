d1ce91f9f7df4518d32d3959ba7cb4bb829e2303
Aslan A <as.ashabokov98@gmail.com>
2022-10-14 23:02:47 +0300

    Backend

---
+++ src/backend/logger.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+
+class GrafanaLogger:
+
+    def __init__(self) -> None:
+        pass
+
+    def error(self, msg: str) -> None:
+        pass
+
+    def info(slef, msg: str) -> None:
+        pass
+
+    def debug(slef, msg: str) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    raise NotImplementedError()
---
+++ src/backend/saver.py
@@ -0,0 +1,17 @@
+import os
+
+import loguru
+import yaml
+
+
+class Drive:
+
+    def __init__(self) -> None:
+        pass
+
+    def send(self, data) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    raise NotImplementedError()
